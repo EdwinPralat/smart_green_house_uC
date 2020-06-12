877840f30802999b4e401c753513ef7d29a0e91e
Karolina Suliga <karolina.suliga98@gmail.com>
2020-06-12 22:52:56 +0200

    added service of temp and humidity sensor(not working perfectly)

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <msp430.h>
-#include <stdio.h>
+#include <intrinsics.h>
+
+int dht[40] = {[0 ... 39] = 5};
+int i;
+int check_sum = 0;
 
 void clock_init()
 {
@@ -11,7 +15,19 @@ void clock_init()
 
 void gpio_init()
 {
-    P1DIR |= 0x01;  // Set P1.0 to output direction
+    P1DIR |= BIT0;  // Set P1.0 to output direction
+    P1DIR |= BIT7;  // Set P1.7 to output direction
+    P1OUT != BIT7;  // Set P1.7 to HIGH
+//    P1SEL |= 0x80;
+//    P1SEL2 &= ~(0x80);
+    P1OUT &= ~BIT0;
+
+//    P1DIR &= ~BIT3;
+//    P1REN |= BIT3;
+////    P1IES |= BIT3;
+////    P1IE |= BIT3;
+//    P1IFG &= ~BIT3;
+
 };
 
 void timer_init()
@@ -26,26 +42,21 @@ void interruptions_init()
     WDTCTL = WDTPW | WDTHOLD; // Stop watchdog timer
     _BIS_SR(CPUOFF + GIE); // sleep untill interupt
 };
-//
-//#pragma vector=0xFFF2
-//#pragma vector=0xFFFA
-//__interrupt void TIMER0_A0_ISR()
-//{
-////    switch(TA1IV)
-////    {
-////        case TA1IV_TAIFG:
-//    P1OUT ^= 0x01;
-//    TA0CCR0 += 32000-1;
-//    TA0CTL &= ~TAIFG;
-//    //            break;
-////        default:
-////            _never_executed();
-//
-////    }
-//};
-//
-//
 
+void get_temp_and_humidity()
+{
+    P1OUT &= ~BIT7;
+    _delay_cycles(5000);
+    P1OUT |= BIT7;
+    _delay_cycles(5);
+    P1OUT &= ~BIT7;
+    P1DIR &= ~BIT7; // Set P1.7 to input direction
+
+    i = 0;
+    check_sum = 0;
+    P1IE |= BIT7;
+    P1IES &= ~BIT7;
+}
 
 
 int main(void)
@@ -63,7 +74,37 @@ int main(void)
 #pragma vector=TIMER1_A0_VECTOR
 __interrupt void TIMER1_A0_ISR(void)
 {
-    P1OUT ^= 0x01;
-    puts('hi');
+//    TA1CCTL0 &= ~BIT0;
+//    TACTL &= ~BIT0;
+    _BIS_SR(GIE); // sleep untill interupt
+
+    get_temp_and_humidity();
 }
 
+#pragma vector=PORT1_VECTOR
+__interrupt void PORT1_ISR(void)
+{
+    if(i != 0)
+    {
+        _delay_cycles(9);
+        if( (P1IN & BIT7) == BIT7)
+        {
+            dht[i-1] = 1;
+            check_sum += 1;
+        }
+        else
+            dht[i-1] = 0;
+    }
+    if(i ==  41)
+    {
+        P1IE &= ~(BIT7);
+        P1DIR |= BIT7;
+        P1OUT |= BIT7;
+        P1OUT ^= BIT0;
+        TA1CCTL0 |= CCIE;
+    }
+
+    i++;
+    P1IFG &= ~BIT7;
+
+}
